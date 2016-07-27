2b31e0ea23a69a302fa7ddf1269afa836083e289
Marnix Looijmans <MJ.Looijmans@me.com>
2016-07-27 11:40:34 +0200

    Add jQuery for dynamic behavior.

---
+++ script.js
@@ -0,0 +1,22 @@
+/**
+ * Copyright (C) 2016 Marnix Looijmans.
+ * All Rights Reserved.
+ *
+ * This code is subject to the XYZ License.
+ * You may use, distribute and modify this code
+ * under the terms of the XYZ license.
+ */
+
+// Listen for clicks on the reset button.
+$(".btn-reset").click(function() {
+    // Get the default URL from the hidden input field.
+    var defaultURL = $("#field-default-url").val();
+    // Set the visible input field to the default URL.
+    $("#field-result-url").val(defaultURL);
+});
+
+// Listen for clicks on the return button.
+$(".btn-return").click(function() {
+    // Redirect to the home page.
+    window.location.href = "/";
+});
